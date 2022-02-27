0e796ac46b029bc1124963f1fdd6c8ef23108ffb
nferraro-roofing <nick.g.ferraro@gmail.com>
2022-02-27 13:03:35 -0500

    Refactor GitHub CI/CD workflows

--- src/test/java/roofing/coffee/maker/busses/ClockTest.java
+++ src/test/java/roofing/coffee/maker/busses/ClockTest.java
@@ -1,17 +1,10 @@
 package roofing.coffee.maker.busses;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Test;
-import roofing.coffee.maker.CoffeeMaker;
-import roofing.coffee.maker.CoffeeMakerCreator;
+import roofing.coffee.maker.TestTimeCoffeeMakerCreator;
 import roofing.coffee.maker.busses.Clock.ClockBuilder;
 import roofing.coffee.maker.components.BrewButton;
-import roofing.coffee.maker.plugins.properties.CoffeeMakerProperties;
-import roofing.coffee.maker.plugins.properties.CoffeeMakerProperties.ClockProps;
-import roofing.coffee.maker.plugins.properties.CoffeeMakerProperties.PotProps;
-import roofing.coffee.maker.plugins.properties.CoffeeMakerProperties.ReservoirProps;
-import roofing.coffee.maker.plugins.properties.CoffeeMakerProperties.WarmerPlateProps;
 
 /**
  * Tests for Clock logic in isolation.
@@ -41,7 +34,7 @@ class ClockTest {
         // Given
         ClockBuilder subject =
                 Clock.builder()
-                        .coffeeMaker(createDummyCoffeeMaker())
+                        .coffeeMaker(TestTimeCoffeeMakerCreator.create())
                         .bus(new Bus(new BrewButton())); // At least one BusComponent so the
                                                          // toString() message is meaningful. Select
                                                          // BrewButton because it's the simplest
@@ -54,18 +47,6 @@ class ClockTest {
         assertEquals(
                 "Clock.ClockBuilder(bus=Bus(synchedComponents=[BrewButton(brewState=NOT_REQUESTED)]), coffeeMaker=CoffeeMaker(reservoir=WaterReservoir(ticksPerCupBrewed=1, maxCapacityCups=11, cupsOfWater=0, isBrewing=false, ticksSinceLastCupBrewed=0), button=BrewButton(brewState=NOT_REQUESTED), pot=CoffeePot(maxCapacityCups=10, ticksPerCupBrewed=1, cupsOfCoffee=0, ticksSinceLastCupBrewed=0), warmer=WarmerPlate(stayHotTickLimit=10, cyclesAfterBrewStopped=0, hasPot=true, isHot=false)))",
                 actual);
-    }
-
-    CoffeeMaker createDummyCoffeeMaker() {
-        ClockProps clock = new ClockProps(60L, TimeUnit.SECONDS);
-        PotProps pot = new PotProps(10);
-        ReservoirProps reservoir = new ReservoirProps(1);
-        WarmerPlateProps warmerPlate = new WarmerPlateProps(10);
-
-        // When
-        CoffeeMakerProperties props = new CoffeeMakerProperties(clock, pot, reservoir, warmerPlate);
-
-        return CoffeeMakerCreator.create(props);
     }
 
 }
